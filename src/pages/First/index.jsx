417e5e17f9baf571e2ea695e3d625b2c18701de8
이경은 <2kyung19@gmail.com>
2022-01-24 22:34:30 +0900

    feat : 절대경로 설정

---
+++ src/pages/First/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const First = () => {
+  return <div className="App">hello</div>;
+};
+
+export default First;
---
+++ src/pages/Second/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Second = () => {
+  return <div className="App">world!</div>;
+};
+
+export default Second;
